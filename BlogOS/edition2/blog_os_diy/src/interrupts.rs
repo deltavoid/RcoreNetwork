9a40f59f06eb0dfbebe2283d774e40851d8f9c0e
zqy <850074816@qq.com>
2019-01-14 11:14:47 +0800

    add blog_os_diy with keyboard interrupt

--- BlogOS/edition2/blog_os_diy/src/interrupts.rs
+++ BlogOS/edition2/blog_os_diy/src/interrupts.rs
@@ -20,6 +20,9 @@ lazy_static! {
 
         idt[usize::from(TIMER_INTERRUPT_ID)]
             .set_handler_fn(timer_interrupt_handler); // new
+        
+        idt[usize::from(KEYBOARD_INTERRUPT_ID)]
+            .set_handler_fn(keyboard_interrupt_handler);
 
         idt
     };
@@ -66,4 +69,37 @@ extern "x86-interrupt" fn timer_interrupt_handler(
 {
     print!(".");
     unsafe { PICS.lock().notify_end_of_interrupt(TIMER_INTERRUPT_ID) }
+}
+
+
+pub const KEYBOARD_INTERRUPT_ID: u8 = PIC_1_OFFSET + 1; // new
+
+
+
+extern "x86-interrupt" fn keyboard_interrupt_handler(
+    _stack_frame: &mut ExceptionStackFrame)
+{
+    use x86_64::instructions::port::Port;
+    use pc_keyboard::{Keyboard, ScancodeSet1, DecodedKey, layouts};
+    use spin::Mutex;
+
+    lazy_static! {
+        static ref KEYBOARD: Mutex<Keyboard<layouts::Us104Key, ScancodeSet1>> =
+            Mutex::new(Keyboard::new(layouts::Us104Key, ScancodeSet1));
+    }
+
+    let mut keyboard = KEYBOARD.lock();
+    let port = Port::new(0x60);
+
+    let scancode: u8 = unsafe { port.read() };
+    if let Ok(Some(key_event)) = keyboard.add_byte(scancode) {
+        if let Some(key) = keyboard.process_keyevent(key_event) {
+            match key {
+                DecodedKey::Unicode(character) => print!("{}", character),
+                DecodedKey::RawKey(key) => print!("{:?}", key),
+            }
+        }
+    }
+
+    unsafe { PICS.lock().notify_end_of_interrupt(KEYBOARD_INTERRUPT_ID) }
 }
