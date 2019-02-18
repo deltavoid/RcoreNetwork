69b71c670c1b103bf432547e6d8f720febf7fd83
zqy <850074816@qq.com>
2019-02-18 14:00:11 +0800

    exception

--- BlogOS/blog_os_diy/src/main.rs
+++ BlogOS/blog_os_diy/src/main.rs
@@ -7,12 +7,42 @@ use blog_os_diy::println;
 use blog_os_diy::print;
 
 
-
 /// This function is the entry point, since the linker looks for a function
 /// named `_start` by default.
+//#[cfg(not(test))]
+//#[no_mangle] // don't mangle the name of this function
+//pub extern "C" fn _start(boot_info: &'static BootInfo) -> ! {
+//    println!("Hello World{}", "!");
+//
+//    blog_os_diy::gdt::init_gdt();
+//    blog_os_diy::interrupts::init_idt();
+//    unsafe {
+//        blog_os_diy::interrupts::PICS.lock().initialize()
+//    };
+//    x86_64::instructions::interrupts::enable();
+//
+//    //blog_os_diy::trigger_a_page_fault();
+//
+//    use blog_os_diy::memory::{self, translate_addr};
+//
+//    use blog_os_diy::memory::{create_example_mapping, EmptyFrameAllocator};
+//
+//    const LEVEL_4_TABLE_ADDR: usize = 0o_177777_777_777_777_777_0000;
+//    let mut recursive_page_table = unsafe { memory::init(LEVEL_4_TABLE_ADDR) };
+//
+//    create_example_mapping(&mut recursive_page_table, &mut EmptyFrameAllocator);
+//    unsafe { (0x1900 as *mut u64).write_volatile(0xf021f077f065f04e)};
+//
+//    println!("It did not crash!");
+//    blog_os_diy::hlt_loop();
+//}
+
+use bootloader::{bootinfo::BootInfo, entry_point};
+
+entry_point!(kernel_main);
+
 #[cfg(not(test))]
-#[no_mangle] // don't mangle the name of this function
-pub extern "C" fn _start() -> ! {
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
     println!("Hello World{}", "!");
 
     blog_os_diy::gdt::init_gdt();
@@ -22,23 +52,24 @@ pub extern "C" fn _start() -> ! {
     };
     x86_64::instructions::interrupts::enable();
 
-    //blog_os_diy::trigger_a_page_fault();
+
 
     use blog_os_diy::memory::{self, translate_addr};
-
     use blog_os_diy::memory::{create_example_mapping, EmptyFrameAllocator};
+    use x86_64::structures::paging::{PageTable, RecursivePageTable};
 
-    const LEVEL_4_TABLE_ADDR: usize = 0o_177777_777_777_777_777_0000;
-    let mut recursive_page_table = unsafe { memory::init(LEVEL_4_TABLE_ADDR) };
+    let mut recursive_page_table = unsafe {
+        memory::init(boot_info.p4_table_addr as usize)
+    };
+    // new
+    let mut frame_allocator = memory::init_frame_allocator(&boot_info.memory_map);
 
-    create_example_mapping(&mut recursive_page_table, &mut EmptyFrameAllocator);
-    unsafe { (0x1900 as *mut u64).write_volatile(0xf021f077f065f04e)};
+    blog_os_diy::memory::create_example_mapping(&mut recursive_page_table, &mut frame_allocator);
+    unsafe { (0xdeadbeaf900 as *mut u64).write_volatile(0xf021f077f065f04e)};
 
     println!("It did not crash!");
-    blog_os_diy::hlt_loop(); 
+    blog_os_diy::hlt_loop();
 }
-
-
 
 /// This function is called on panic.
 #[cfg(not(test))]
--- BlogOS/blog_os_diy/src/memory.rs
+++ BlogOS/blog_os_diy/src/memory.rs
@@ -69,3 +69,39 @@ impl FrameAllocator<Size4KiB> for EmptyFrameAllocator {
         None
     }
 }
+
+
+pub struct BootInfoFrameAllocator<I> where I: Iterator<Item = PhysFrame> {
+    frames: I,
+}
+
+impl<I> FrameAllocator<Size4KiB> for BootInfoFrameAllocator<I>
+    where I: Iterator<Item = PhysFrame>
+{
+    fn allocate_frame(&mut self) -> Option<PhysFrame> {
+        self.frames.next()
+    }
+}
+
+
+use bootloader::bootinfo::{MemoryMap, MemoryRegionType};
+
+/// Create a FrameAllocator from the passed memory map
+pub fn init_frame_allocator(
+    memory_map: &'static MemoryMap,
+) -> BootInfoFrameAllocator<impl Iterator<Item = PhysFrame>> {
+    // get usable regions from memory map
+    let regions = memory_map
+        .iter()
+        .filter(|r| r.region_type == MemoryRegionType::Usable);
+    // map each region to its address range
+    let addr_ranges = regions.map(|r| r.range.start_addr()..r.range.end_addr());
+    // transform to an iterator of frame start addresses
+    let frame_addresses = addr_ranges.flat_map(|r| r.into_iter().step_by(4096));
+    // create `PhysFrame` types from the start addresses
+    let frames = frame_addresses.map(|addr| {
+        PhysFrame::containing_address(PhysAddr::new(addr))
+    });
+
+    BootInfoFrameAllocator { frames }
+}
