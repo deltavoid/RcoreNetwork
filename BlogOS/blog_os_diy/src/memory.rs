74a03eebe0c5c73e4ecc79fad89b9d71bcda8645
zqy <850074816@qq.com>
2019-02-18 13:39:31 +0800

    add blog_os memory mapping

--- BlogOS/blog_os_diy/src/memory.rs
+++ BlogOS/blog_os_diy/src/memory.rs
@@ -7,10 +7,24 @@ use x86_64::{VirtAddr, PhysAddr};
 ///
 /// This function is unsafe because it can break memory safety if an invalid
 /// address is passed.
+//pub unsafe fn init(level_4_table_addr: usize) -> RecursivePageTable<'static> {
+//    let level_4_table_ptr = level_4_table_addr as *mut PageTable;
+//    let level_4_table = &mut *level_4_table_ptr;
+//    RecursivePageTable::new(level_4_table).unwrap()
+//}
+
+
 pub unsafe fn init(level_4_table_addr: usize) -> RecursivePageTable<'static> {
-    let level_4_table_ptr = level_4_table_addr as *mut PageTable;
-    let level_4_table = &mut *level_4_table_ptr;
-    RecursivePageTable::new(level_4_table).unwrap()
+    /// Rust currently treats the whole body of unsafe functions as an unsafe
+    /// block, which makes it difficult to see which operations are unsafe. To
+    /// limit the scope of unsafe we use a safe inner function.
+    fn init_inner(level_4_table_addr: usize) -> RecursivePageTable<'static> {
+        let level_4_table_ptr = level_4_table_addr as *mut PageTable;
+        let level_4_table = unsafe { &mut *level_4_table_ptr };
+        RecursivePageTable::new(level_4_table).unwrap()
+    }
+
+    init_inner(level_4_table_addr)
 }
 
 /// Returns the physical address for the given virtual address, or `None` if
@@ -24,4 +38,34 @@ pub fn translate_addr(addr: u64, recursive_page_table: &RecursivePageTable)
     // perform the translation
     let frame = recursive_page_table.translate_page(page);
     frame.map(|frame| frame.start_address() + u64::from(addr.page_offset()))
+}
+
+
+
+use x86_64::structures::paging::{FrameAllocator, PhysFrame, Size4KiB};
+
+pub fn create_example_mapping(
+    recursive_page_table: &mut RecursivePageTable,
+    frame_allocator: &mut impl FrameAllocator<Size4KiB>,
+) {
+    use x86_64::structures::paging::PageTableFlags as Flags;
+
+    let page: Page = Page::containing_address(VirtAddr::new(0x1000));
+    let frame = PhysFrame::containing_address(PhysAddr::new(0xb8000));
+    let flags = Flags::PRESENT | Flags::WRITABLE;
+
+    let map_to_result = unsafe {
+        recursive_page_table.map_to(page, frame, flags, frame_allocator)
+    };
+    map_to_result.expect("map_to failed").flush();
+}
+
+
+/// A FrameAllocator that always returns `None`.
+pub struct EmptyFrameAllocator;
+
+impl FrameAllocator<Size4KiB> for EmptyFrameAllocator {
+    fn allocate_frame(&mut self) -> Option<PhysFrame> {
+        None
+    }
 }
